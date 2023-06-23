48777839130f44fc8a52143a748140e58ec0067e
Susiee03 <susie.yu19990420@gmail.com>
2023-06-22 21:46:56 -0700

    add 3 tests files

--- client/src/tests/HomePage.test.js
+++ client/src/tests/HomePage.test.js
@@ -1,3 +1,35 @@
+// import { render, screen } from "@testing-library/react";
+// import Todos from "../components/Todos";
+// import { MemoryRouter } from "react-router-dom";
+// import { enableFetchMocks } from "jest-fetch-mock";
+// enableFetchMocks();
+
+// jest.mock("@auth0/auth0-react", () => ({
+//   ...jest.requireActual("@auth0/auth0-react"),
+//   Auth0Provider: ({ children }) => children,
+//   useAuth0: () => {
+//     return {
+//       isLoading: false,
+//       user: { sub: "foobar" },
+//       isAuthenticated: true,
+//       loginWithRedirect: jest.fn(),
+//     };
+//   },
+// }));
+
+// jest.mock("../AuthTokenContext", () => ({
+//   useAuthToken: () => {
+//     return { accessToken: "123" };
+//   },
+// }));
+
+// fetch.mockResponse(
+//     JSON.stringify([
+//       { id: 1, content: "hello" },
+//     ])
+//   );
+
+
 import { render, screen } from "@testing-library/react";
 import Home from "../components/HomePage";
 import { MemoryRouter } from "react-router-dom";
@@ -34,7 +66,6 @@ test("renders Home copy and Login Button", () => {
     </MemoryRouter>
   );
 
-  expect(screen.getByText("TODO App")).toBeInTheDocument();
   expect(screen.getByText("Login")).toBeInTheDocument();
 });
 
@@ -50,6 +81,7 @@ test("login button calls loginWithRedirect", async () => {
 
   expect(mockLoginWithRedirect).toHaveBeenCalled();
 });
+
 
 test("renders Enter App button when user is authenticated", () => {
   mockIsAuthenticated = true;
